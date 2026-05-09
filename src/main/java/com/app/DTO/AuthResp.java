5ab52020594b7c76e449afe6de98cbe8220a9a6a
Saurav Singh <sauravchauhan0017@gmail.com>
2026-05-09 23:15:28 +0530

    Fixed AuthResp duplicate constructors

--- src/main/java/com/app/DTO/AuthResp.java
+++ src/main/java/com/app/DTO/AuthResp.java
@@ -1,6 +1,5 @@
 package com.app.DTO;
 
-
 import org.springframework.security.core.Authentication;
 
 import lombok.AllArgsConstructor;
@@ -15,38 +14,9 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthResp {
-	private String message;
-	private String jwt;
-	private Authentication authenticatedDetails;
-	
-	
-	public AuthResp() {
-		
-	}
-	public AuthResp(String message, String jwt, Authentication authenticatedDetails) {
-	
-		this.message = message;
-		this.jwt = jwt;
-		this.authenticatedDetails = authenticatedDetails;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String getJwt() {
-		return jwt;
-	}
-	public void setJwt(String jwt) {
-		this.jwt = jwt;
-	}
-	public Authentication getAuthenticatedDetails() {
-		return authenticatedDetails;
-	}
-	public void setAuthenticatedDetails(Authentication authenticatedDetails) {
-		this.authenticatedDetails = authenticatedDetails;
-	}
-	
-	
+
+    private String message;
+    private String jwt;
+    private Authentication authenticatedDetails;
+
 }
