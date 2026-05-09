3d191908394e0533d34261d6d90648bca49a477c
Saurav Singh <sauravchauhan0017@gmail.com>
2026-05-09 22:59:28 +0530

    Added Docker deployment support

--- src/main/java/com/app/Entity/User.java
+++ src/main/java/com/app/Entity/User.java
@@ -6,10 +6,12 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
+@Table(name = "users")
 @Data
 public class User {
 	@Id
