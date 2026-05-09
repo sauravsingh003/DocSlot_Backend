079b816d911f2c401787dd06448ccad5496acee2
Saurav Singh <sauravchauhan0017@gmail.com>
2026-05-09 23:20:06 +0530

    Fixed AuthResp constructor issue

--- src/main/java/com/app/DTO/AuthResp.java
+++ src/main/java/com/app/DTO/AuthResp.java
@@ -2,21 +2,26 @@ package com.app.DTO;
 
 import org.springframework.security.core.Authentication;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
-@AllArgsConstructor
 public class AuthResp {
 
     private String message;
     private String jwt;
     private Authentication authenticatedDetails;
 
+    public AuthResp() {
+
+    }
+
+    public AuthResp(String message, String jwt, Authentication authenticatedDetails) {
+        this.message = message;
+        this.jwt = jwt;
+        this.authenticatedDetails = authenticatedDetails;
+    }
 }
